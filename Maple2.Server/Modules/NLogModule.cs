c49b5a59ee5214ad4389828f16e36852bdab39bb
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-01-01 23:02:04 -0800

    nlog config through appsettings.json

--- Maple2.Server/Modules/NLogModule.cs
+++ Maple2.Server/Modules/NLogModule.cs
@@ -1,10 +1,22 @@
-﻿using Autofac;
+﻿using System.IO;
+using Autofac;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 
 namespace Maple2.Server.Modules;
 
 internal class NLogModule : Module {
+    private const string APP_SETTINGS = "appsettings.json";
+
+    static NLogModule() {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(APP_SETTINGS, true, true)
+            .Build();
+        NLog.LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+    }
+
     protected override void Load(ContainerBuilder builder) {
         builder.Register(_ => {
                 var factory = new LoggerFactory();
