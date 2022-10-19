9b5d0b0766c67d9afef9dc53382d0cc3e8374f45
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-10-18 23:19:34 -0700

    Basic Python trigger generation

---
+++ Maple2.File.Ingest/Generator/TriggerGenerator.cs
@@ -0,0 +1,135 @@
+﻿using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Xml;
+using Maple2.File.Ingest.Utils;
+using Maple2.File.IO;
+using Maple2.File.IO.Crypto.Common;
+
+namespace Maple2.File.Ingest.Generator;
+
+public class TriggerGenerator {
+    private readonly M2dReader reader;
+
+    public TriggerGenerator(M2dReader xmlReader) {
+        reader = xmlReader;
+    }
+
+    public void Generate() {
+        foreach (PackFileEntry entry in reader.Files.Where(file => file.Name.StartsWith("trigger/"))) {
+            string scriptDir = entry.Name.Split('/', StringSplitOptions.RemoveEmptyEntries)[1];
+            string scriptName = Path.GetFileNameWithoutExtension(entry.Name);
+            Directory.CreateDirectory($"trigger/{scriptDir}");
+            string pyName = $"trigger/{scriptDir}/{scriptName}.py";
+            using var stream = new StreamWriter(pyName);
+            using var writer = new IndentedTextWriter(stream, "  ");
+            writer.WriteLine(@$""""""" {entry.Name} """"""");
+
+            XmlDocument document = reader.GetXmlDocument(entry);
+            XmlNodeList stateNodeList = document.SelectNodes("ms2/state")!;
+            try {
+                foreach (XmlNode importNode in document.SelectNodes("ms2/import")!) {
+                    string path = importNode.Attributes["path"].Value.ToLower();
+                    string importDir = Directory.GetParent(path).Name;
+                    string importName = Path.GetFileNameWithoutExtension(path);
+
+                    writer.WriteLine("import common");
+                    writer.WriteLine($"import {importDir}/{importName}");
+                }
+                writer.WriteLine();
+
+                // Copy node list so that we can remove duplicate states
+                List<XmlNode> stateNodes = stateNodeList.Cast<XmlNode>().ToList();
+                var stateIndex = new Dictionary<string, short>();
+                short index = 0;
+                foreach (XmlNode stateNode in stateNodes.ToList()) {
+                    string name = stateNode.Attributes["name"].Value;
+                    if (stateIndex.ContainsKey(name)) {
+                        Console.WriteLine($"Duplicate state in {entry.Name} ignored and removed: {name}");
+                        stateNodes.Remove(stateNode);
+                    } else {
+                        stateIndex.Add(name, index++);
+                    }
+                }
+
+                var script = new TriggerScript();
+                foreach (XmlNode stateNode in stateNodes) {
+                    TriggerScript.State scriptState = ParseState(stateNode, stateIndex, entry.Name);
+                    script.States.Add(scriptState);
+                }
+
+                script.WriteTo(writer);
+                Console.WriteLine($"Generated {pyName}...");
+            } catch (Exception ex) {
+                Console.WriteLine($"Failed to parse file: {entry.Name} - {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+
+    private static TriggerScript.State ParseState(XmlNode node, Dictionary<string, short> stateIndex, string filePath) {
+        string name = node.Attributes["name"].Value;
+        var onEnter = new List<TriggerScript.Action>();
+        foreach (XmlNode action in node.SelectNodes("onEnter/action")) {
+            onEnter.Add(ParseAction(action));
+        }
+
+        var conditions = new List<TriggerScript.Condition>();
+        foreach (XmlNode condition in node.SelectNodes("condition")) {
+            conditions.Add(ParseCondition(condition, stateIndex, filePath));
+        }
+
+        var onExit = new List<TriggerScript.Action>();
+        foreach (XmlNode action in node.SelectNodes("onExit/action")) {
+            onExit.Add(ParseAction(action));
+        }
+
+        return new TriggerScript.State {
+            Name = name,
+            OnEnter = onEnter,
+            Conditions = conditions,
+            OnExit = onExit,
+        };
+    }
+
+    private static TriggerScript.Action ParseAction(XmlNode node) {
+        var args = new Dictionary<string, string>();
+        foreach (XmlAttribute attribute in node.Attributes) {
+            args[attribute.Name] = attribute.Value;
+        }
+
+        Debug.Assert(args.Remove("name", out string? name), "Unable to find name param");
+        return new TriggerScript.Action {
+            Name = name,
+            Args = args,
+        };
+    }
+
+    private static TriggerScript.Condition ParseCondition(XmlNode node, Dictionary<string, short> stateIndex, string filePath) {
+        var args = new Dictionary<string, string>();
+        foreach (XmlAttribute attribute in node.Attributes) {
+            args[attribute.Name] = attribute.Value;
+        }
+        Debug.Assert(args.Remove("name", out string? name), "Unable to find name param");
+        bool negated = name.StartsWith('!');
+
+        var actions = new List<TriggerScript.Action>();
+        foreach (XmlNode action in node.SelectNodes("action")) {
+            actions.Add(ParseAction(action));
+        }
+
+        string? transition = node.SelectSingleNode("transition")?.Attributes?["state"]?.Value;
+        // if (transition == null || !stateIndex.ContainsKey(transition)) {
+        //     Console.WriteLine($"Script {filePath} Missing transition: {transition}");
+        //     Console.WriteLine($"- {string.Join(",", stateIndex.Keys)}");
+        //     transition = null;
+        // }
+
+        return new TriggerScript.Condition {
+            Name = name.TrimStart('!'),
+            Negated = negated,
+            Args = args,
+            Actions = actions,
+            Transition = transition,
+        };
+    }
+}
---
+++ Maple2.File.Ingest/Utils/TriggerScript.cs
@@ -0,0 +1,92 @@
+﻿using System.CodeDom.Compiler;
+
+namespace Maple2.File.Ingest.Utils;
+
+internal class TriggerScript {
+    public readonly IList<State> States;
+
+    public TriggerScript() {
+        States = new List<State>();
+    }
+
+    public class State {
+        public string Name = string.Empty;
+        public IList<Action> OnEnter = new List<Action>();
+        public IList<Condition> Conditions = new List<Condition>();
+        public IList<Action> OnExit = new List<Action>();
+
+        public void WriteTo(IndentedTextWriter writer) {
+            writer.WriteLine($"class {Name}(State):");
+            writer.Indent++;
+            if (OnEnter.Count > 0) {
+                writer.WriteLine("def on_enter():");
+                writer.Indent++;
+                foreach (Action action in OnEnter) {
+                    action.WriteTo(writer);
+                }
+                writer.Indent--;
+                WriteBlankLine(writer);
+            }
+            if (Conditions.Count > 0) {
+                writer.WriteLine("def on_tick():");
+                writer.Indent++;
+                foreach (Condition condition in Conditions) {
+                    condition.WriteTo(writer);
+                }
+                writer.Indent--;
+                WriteBlankLine(writer);
+            }
+            if (OnExit.Count > 0) {
+                writer.WriteLine("def on_exit():");
+                writer.Indent++;
+                foreach (Action action in OnExit) {
+                    action.WriteTo(writer);
+                }
+                writer.Indent--;
+                WriteBlankLine(writer);
+            }
+            writer.Indent--;
+            WriteBlankLine(writer);
+        }
+    }
+
+    public class Action {
+        public string Name = string.Empty;
+        public IDictionary<string, string> Args = new Dictionary<string, string>();
+
+        public void WriteTo(IndentedTextWriter writer) {
+            writer.WriteLine($"{Name}({string.Join(", ", Args.Select(arg => $"{arg.Key}={arg.Value}"))})");
+        }
+    }
+
+    public class Condition {
+        public string Name = string.Empty;
+        public bool Negated;
+        public IDictionary<string, string> Args = new Dictionary<string, string>();
+        public IList<Action> Actions = new List<Action>();
+        public string? Transition;
+
+        public void WriteTo(IndentedTextWriter writer) {
+            writer.WriteLine($"if {(Negated ? "not " : "")}{Name}({string.Join(", ", Args.Select(arg => $"{arg.Key}={arg.Value}"))}):");
+            writer.Indent++;
+            foreach (Action action in Actions) {
+                action.WriteTo(writer);
+            }
+            writer.WriteLine($"return '{Transition}'");
+            writer.Indent--;
+        }
+    }
+
+    public void WriteTo(IndentedTextWriter writer) {
+        foreach (State state in States) {
+            state.WriteTo(writer);
+        }
+    }
+
+    private static void WriteBlankLine(IndentedTextWriter writer) {
+        int indent = writer.Indent;
+        writer.Indent = 0;
+        writer.WriteLine();
+        writer.Indent = indent;
+    }
+}
