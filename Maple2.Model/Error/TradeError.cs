1b015b61e8b6528ce41c9424976f47d39b16fb43
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-10-25 00:35:25 -0700

    Add TradePacket structs

---
+++ Maple2.Model/Error/TradeError.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum TradeError : byte {
+    [Description("Trade failed.")]
+    s_trade_error_system = 0,
+    [Description("The trade request failed because the target is too far away.")]
+    s_trade_error_distance = 1,
+    [Description("{0} has already received a trade request.")]
+    s_trade_error_already_request = 2,
+    [Description("Trade in progress.")]
+    s_trade_error_trading_now = 3,
+    [Description("Your offer has already been finalized.")]
+    s_trade_error_latched = 5,
+    [Description("Trading is not possible with this target.")]
+    s_trade_error_decline = 6,
+    [Description("All slots in the trade window are full. Remove an item, or make deliver the item through a separate trade.")]
+    s_trade_error_itemcount = 7,
+    [Description("The other player's inventory is full.")]
+    s_trade_error_slotcount = 8,
+    [Description("The trade has been canceled because there are no items to trade.")]
+    s_trade_error_itemnone = 9,
+    [Description("You cannot trade during PvP.")]
+    s_trade_error_pvp = 10,
+    [Description("This can't be done here.")]
+    s_trade_error_mapLimit = 11,
+    [Description("{0} failed to respond to your trade request.")]
+    s_trade_error_timeout = 12,
+    [Description("Confirm the trade amount.")]
+    s_trade_error_invalid_meso = 13,
+    [Description("Not enough mesos.")]
+    s_trade_error_meso = 14,
+    [Description("{0} is in protected mode.")]
+    s_trade_error_target_property_protection_time = 15,
+    [Description("Trading is not possible due to {0}'s fatigue.")]
+    s_trade_error_target_fatigue_penalty = 16,
+    [Description("")]
+    unknown = 17,
+    [Description("You cannot trade if the highest level character in your account is less than {0}.")]
+    s_trade_error_restricted_userlevel_max = 18,
+    [Description("You cannot trade with this player because their highest level character is less than {0}.")]
+    s_trade_error_restricted_target_userlevel_max = 19,
+    [Description("You must have a character that&apos;s Lv. {1} or higher to trade {0}.")]
+    s_trade_error_send_restricted_by_user_level_ex = 20,
+    [Description("The person you are trading with must have a character that&apos;s Lv. {1} or higher to trade {0}.")]
+    s_trade_error_recv_restricted_by_user_level_ex = 21,
+
+    [Description("System Error: Community")]
+    s_trade_error = byte.MaxValue,
+}
---
+++ Maple2.Server.Game/Packets/TradePacket.cs
@@ -0,0 +1,141 @@
+﻿using Maple2.Model.Game;
+using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.Packets;
+using Maple2.Tools.Extensions;
+
+namespace Maple2.Server.Game.Packets;
+
+public static class TradePacket {
+    private enum Command : byte {
+        Request = 0,
+        Error = 1,
+        Acknowledge = 2,
+        Decline = 4,
+        StartTrade = 5,
+        EndTrade = 6,
+        AddItem = 8,
+        RemoveItem = 9,
+        SetMesos = 10,
+        Finalize = 11,
+        UnFinalize = 12,
+        Complete = 13,
+        AlreadyRequest = 14,
+    }
+
+    public static ByteWriter Request(Player player) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Request);
+        pWriter.WriteUnicodeString(player.Character.Name);
+        pWriter.WriteLong(player.Character.Id);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Error() {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Error);
+        pWriter.WriteByte();
+        pWriter.WriteUnicodeString();
+        pWriter.WriteInt();
+        pWriter.WriteInt();
+
+        return pWriter;
+    }
+
+    public static ByteWriter Acknowledge() {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Acknowledge);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Decline(string name) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Decline);
+        pWriter.WriteUnicodeString(name);
+
+        return pWriter;
+    }
+
+    public static ByteWriter StartTrade(long characterId) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.StartTrade);
+        pWriter.WriteLong(characterId);
+
+        return pWriter;
+    }
+
+    public static ByteWriter EndTrade(bool success) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.EndTrade);
+        pWriter.WriteBool(success);
+
+        return pWriter;
+    }
+
+    public static ByteWriter AddItem(bool index, Item item) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.AddItem);
+        pWriter.WriteBool(index);
+        pWriter.WriteInt(item.Id);
+        pWriter.WriteLong(item.Uid);
+        pWriter.WriteInt(item.Rarity);
+        pWriter.WriteInt(item.Slot);
+        pWriter.WriteInt(item.Amount);
+        pWriter.WriteInt(item.Slot);
+        pWriter.WriteClass<Item>(item);
+
+        return pWriter;
+    }
+
+    public static ByteWriter RemoveItem(bool index, int tradeSlot, long itemUid) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.RemoveItem);
+        pWriter.WriteBool(index);
+        pWriter.WriteInt(tradeSlot);
+        pWriter.WriteLong(itemUid);
+
+        return pWriter;
+    }
+
+    public static ByteWriter SetMesos(bool index, long mesos) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.SetMesos);
+        pWriter.WriteBool(index);
+        pWriter.WriteLong(mesos);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Finalize(bool index) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Finalize);
+        pWriter.WriteBool(index);
+
+        return pWriter;
+    }
+
+    public static ByteWriter UnFinalize(bool index) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.UnFinalize);
+        pWriter.WriteBool(index);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Complete(bool index) {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.Complete);
+        pWriter.WriteBool(index);
+
+        return pWriter;
+    }
+
+    public static ByteWriter AlreadyRequest() {
+        var pWriter = Packet.Of(SendOp.Trade);
+        pWriter.Write<Command>(Command.AlreadyRequest);
+
+        return pWriter;
+    }
+}
