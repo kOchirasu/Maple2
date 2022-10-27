21e600c9c5702e9c395c94d300e4d69be3979e7d
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-10-27 00:14:22 -0700

    Add StorageInventoryPacket structs

---
+++ Maple2.Model/Error/StorageInventoryError.cs
@@ -0,0 +1,35 @@
+﻿// ReSharper disable InconsistentNaming, IdentifierTypo
+
+using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum StorageInventoryError {
+    [Description("The item amounts do not match.")]
+    s_item_err_invalid_count = 10,
+    [Description("This item cannot be stored here.")]
+    s_item_err_invaild_store_type = 12,
+    [Description("Storage is full.")]
+    s_item_err_store_full = 13,
+    [Description("This cannot be expanded any further.")]
+    s_store_err_expand_max = 14,
+    [Description("Not enough merets.")]
+    s_cannot_charge_merat = 15,
+    [Description("This item cannot be stored.")]
+    s_item_err_binditem = 16,
+    [Description("An item belonging to a specific character can only be retrieved by that character.")]
+    s_item_err_binditem_store_out = 17,
+    [Description("Meso storage is not possible with this safe.")]
+    s_store_err_deposit_disable_type = 18,
+    [Description("Confirm the amount entered.")]
+    s_store_err_deposit_invalid_money = 19,
+    [Description("Up to {0} mesos can be stored.")]
+    s_store_err_deposit_max_money = 20,
+    [Description("Not enough funds.")]
+    s_cashshop_lack_balance = 21,
+    [Description("This item can only be retrieved by the character that stored it.")]
+    s_item_err_moveDisableitem_store_out = 22,
+
+    [Description("Bank safe error code: {0}")]
+    s_store_err_code = byte.MaxValue,
+}
---
+++ Maple2.Server.Game/Packets/StorageInventoryPacket.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using Maple2.Model.Error;
+using Maple2.Model.Game;
+using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.Packets;
+using Maple2.Tools.Extensions;
+
+namespace Maple2.Server.Game.Packets;
+
+public static class StorageInventoryPacket {
+    private enum Command : byte {
+        Add = 0,
+        Remove = 1,
+        Move = 2,
+        UpdateMesos = 3,
+        SlotsUsed = 4,
+        Load = 5,
+        Reload = 8,
+        Update = 9,
+        Reset = 11,
+        SlotsExpanded = 13,
+        OpenDialog = 14,
+        Error = 16,
+    }
+
+    public static ByteWriter Add(Item item) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Add);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteInt(item.Id);
+        pWriter.WriteLong(item.Uid);
+        pWriter.WriteShort(item.Slot);
+        pWriter.WriteInt(item.Rarity);
+        pWriter.WriteClass<Item>(item);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Remove(long uid) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Remove);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteLong(uid);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Move(long dstUid, short srcSlot, long srcUid, short dstSlot) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Move);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteLong(dstUid);
+        pWriter.WriteShort(srcSlot);
+        pWriter.WriteLong(srcUid);
+        pWriter.WriteShort(dstSlot);
+
+        return pWriter;
+    }
+
+    public static ByteWriter UpdateMesos(long mesos) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.UpdateMesos);
+        pWriter.WriteLong(mesos);
+
+        return pWriter;
+    }
+
+    public static ByteWriter SlotsUsed(short slotsUsed) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.SlotsUsed);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteShort(slotsUsed);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Load(ICollection<Item> items) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Load);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteShort((short)items.Count);
+        foreach (Item item in items) {
+            pWriter.WriteInt(item.Id);
+            pWriter.WriteLong(item.Uid);
+            pWriter.WriteShort(item.Slot);
+            pWriter.WriteInt(item.Rarity);
+            pWriter.WriteClass<Item>(item);
+        }
+
+        return pWriter;
+    }
+
+    // Used for sorting
+    public static ByteWriter Reload(ICollection<Item> items) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Reload);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteShort((short)items.Count);
+        foreach (Item item in items) {
+            pWriter.WriteInt(item.Id);
+            pWriter.WriteLong(item.Uid);
+            pWriter.WriteShort(item.Slot);
+            pWriter.WriteInt(item.Rarity);
+            pWriter.WriteClass<Item>(item);
+        }
+
+        return pWriter;
+    }
+
+    public static ByteWriter Update(long uid, int remaining) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Update);
+        pWriter.WriteLong(); // Unknown (0)
+        pWriter.WriteLong(uid);
+        pWriter.WriteInt(remaining);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Reset() {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Reset);
+
+        return pWriter;
+    }
+
+    public static ByteWriter SlotsExpanded(int expansion) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.SlotsExpanded);
+        pWriter.WriteInt(expansion);
+
+        return pWriter;
+    }
+
+    public static ByteWriter OpenDialog() {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.OpenDialog);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Error(StorageInventoryError error) {
+        var pWriter = Packet.Of(SendOp.StorageInventory);
+        pWriter.Write<Command>(Command.Error);
+        pWriter.Write<StorageInventoryError>(error);
+
+        return pWriter;
+    }
+}
