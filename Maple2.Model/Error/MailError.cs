1d18b7df1fb6e4dfcd84c9b08797dc485417389e
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-11-01 22:00:13 -0700

    Add Mail packet structs

--- Maple2.Model/Error/MailError.cs
+++ Maple2.Model/Error/MailError.cs
@@ -1,3 +1,53 @@
-﻿namespace Maple2.Model.Error; 
+﻿// ReSharper disable InconsistentNaming, IdentifierTypo
 
-public enum MailError {  }
+using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum MailError : byte {
+    none = 0,
+    [Description("Empty {0} spaces in your inventory.")]
+    s_err_job_inventory_full = 1,
+    [Description("The item amounts do not match.")]
+    s_mail_error_attachcount = 2, // also 5
+    [Description("This item cannot be sent.")]
+    s_mail_error_cannot_attach_item = 3, // also 4
+    [Description("Mail has not been sent.")]
+    s_mail_error_sendmail = 12,
+    [Description("This mail has been read already.")]
+    s_mail_error_alreadyread = 16,
+    [Description("The attached item on this mail has already been retrieved.")]
+    s_mail_error_already_receive = 17, // also 21
+    [Description("The item amounts do not match.")]
+    s_mail_error_attachcount_effect17 = 20, // + effect(17)
+    [Description("The item amounts do not match.")]
+    s_mail_error_attachcount_effect18 = 22, // + effect(18)
+    [Description("The sale has ended.")]
+    s_mail_error_ad_expired = 23,
+    [Description("Mail creation failed.")]
+    s_mail_error_createmail = 24,
+    [Description("The sender and recipient are the same player.")]
+    s_mail_error_recipient_equal_sender = 25,
+    [Description("Not enough mesos.")]
+    s_err_lack_meso = 26,
+    [Description("The mail recipient has been blocked. Please unblock and try again.")]
+    s_mail_error_block_from_me = 27,
+    [Description("The mail recipient has blocked you. You will not be able to send them mail.")]
+    s_mail_error_block_from_other = 28,
+    [Description("Mail cannot be sent.")]
+    s_mail_error_admin_character = 29,
+    [Description("GMs cannot send mail directly to players. Please use the proper GM tool.")]
+    s_mail_error_from_admin_to_user = 30,
+    [Description("The title and/or text contains a forbidden word.")]
+    s_mail_error_bancheck = 31,
+    [Description("Your mail privileges have been suspended.")]
+    s_mail_error_admin_block = 32,
+    [Description("The game will not run due to fatigue time.")]
+    s_anti_addiction_cannot_receive = 33,
+
+    // Custom Error Codes
+    mail_not_found = 44,
+
+    [Description("System Error: Mail. p = {0}, code = {1}")]
+    s_mail_error = byte.MaxValue,
+}
--- Maple2.Server.Game/Packets/MailPacket.cs
+++ Maple2.Server.Game/Packets/MailPacket.cs
@@ -1,5 +1,169 @@
-﻿namespace Maple2.Server.Game.Packets; 
+﻿using System;
+using System.Collections.Generic;
+using Maple2.Model.Error;
+using Maple2.Model.Game;
+using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.Packets;
+using Maple2.Tools.Extensions;
 
-public class MailPacket {
-    
+namespace Maple2.Server.Game.Packets;
+
+public static class MailPacket {
+    private enum Command : byte {
+        Load = 0,
+        Send = 1,
+        Read = 2,
+        Returned = 3,
+        Collect = 10,
+        CollectRead = 11,
+        AdBill = 12,
+        Deleted = 13,
+        Notify = 14,
+        NotifyTemporary = 15,
+        StartList = 16,
+        EndList = 17,
+        Error = 20,
+        Gift = 22,
+    }
+
+    public static ByteWriter Load(ICollection<Mail> mails) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Load);
+
+        pWriter.WriteInt(mails.Count);
+        foreach (Mail mail in mails) {
+            pWriter.WriteClass<Mail>(mail);
+        }
+
+        return pWriter;
+    }
+
+    public static ByteWriter Send(long mailId) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Send);
+        pWriter.WriteLong(mailId);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Read(Mail mail) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Read);
+        pWriter.WriteLong(mail.Id);
+        pWriter.WriteLong(mail.ReadTime);
+
+        return pWriter;
+    }
+
+    // s_mail_return
+    // - Mail has been returned.
+    public static ByteWriter Returned() {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Returned);
+        pWriter.WriteLong();
+
+        return pWriter;
+    }
+
+    public static ByteWriter Collect(long mailId, bool success = true) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Collect);
+        pWriter.WriteLong(mailId);
+        pWriter.WriteBool(success);
+        if (success) {
+            pWriter.WriteByte(0); // assert(<= 4)
+            pWriter.WriteLong(DateTimeOffset.UtcNow.ToUnixTimeSeconds()); // Collection time
+        }
+
+        return pWriter;
+    }
+
+    public static ByteWriter CollectRead(Mail mail) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.CollectRead);
+        pWriter.WriteLong(mail.Id);
+        pWriter.WriteLong(mail.ReadTime);
+
+        return pWriter;
+    }
+
+    public static ByteWriter AdBill(Mail mail) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.AdBill);
+        pWriter.WriteLong(mail.Id);
+        pWriter.WriteLong(); // timestamp
+
+        return pWriter;
+    }
+
+    public static ByteWriter Deleted(long mailId) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Deleted);
+        pWriter.WriteLong(mailId);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Notify(int unreadCount, int count, bool notify = true) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Notify);
+        pWriter.WriteInt(unreadCount);
+        pWriter.WriteBool(notify);
+        pWriter.WriteInt(count);
+
+        return pWriter;
+    }
+
+    // s_mail_period_item_include
+    // - Mail has arrived with a temporary item attached
+    // s_mail_period_item_include_chat
+    // - Mail has arrived with a temporary item attached.\nBe sure to claim it before it expires!
+    public static ByteWriter NotifyTemporary() {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.NotifyTemporary);
+
+        return pWriter;
+    }
+
+    public static ByteWriter StartList() {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.StartList);
+
+        return pWriter;
+    }
+
+    public static ByteWriter EndList() {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.EndList);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Error(MailError error, byte code = 0) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Error);
+        pWriter.WriteByte(code); // used in default case
+        pWriter.Write<MailError>(error);
+
+        return pWriter;
+    }
+
+    // s_mail_notify_recieve_gift
+    // - You received a gift from {0}!
+    // s_mail_get_gift_item
+    // - {1} has sent you a gift. Check your mailbox.
+    public static ByteWriter Gift(long mailId) {
+        var pWriter = Packet.Of(SendOp.Mail);
+        pWriter.Write<Command>(Command.Gift);
+        pWriter.WriteUnicodeString();
+        pWriter.WriteByte();
+        pWriter.WriteInt();
+        pWriter.WriteByte();
+        pWriter.WriteInt();
+        pWriter.WriteString();
+        pWriter.WriteUnicodeString();
+
+        return pWriter;
+    }
 }
