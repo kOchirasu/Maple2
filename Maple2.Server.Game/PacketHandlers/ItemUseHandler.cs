fbda0b0cc19758377163c0776e8366ccd1dc2f55
Zin <62830952+Zintixx@users.noreply.github.com>
2022-11-04 22:50:17 -0700

    Implement Storybook Use Items

--- Maple2.Server.Game/PacketHandlers/ItemUseHandler.cs
+++ Maple2.Server.Game/PacketHandlers/ItemUseHandler.cs
@@ -2,6 +2,7 @@
 using Maple2.PacketLib.Tools;
 using Maple2.Server.Core.Constants;
 using Maple2.Server.Core.PacketHandlers;
+using Maple2.Server.Game.Packets;
 using Maple2.Server.Game.Session;
 
 namespace Maple2.Server.Game.PacketHandlers;
@@ -18,5 +19,23 @@ public class ItemUseHandler : PacketHandler<GameSession> {
             Logger.Warning("RequestItemUse for invalid item:{ItemUid}", itemUid);
             return;
         }
+
+        switch (item.Metadata.Function?.Name)
+        {
+            case "StoryBook":
+                HandleStoryBook(session, item);
+                break;
+            default:
+                Logger.Warning("Unhandled item function: {item.Metadata.Function?.Name}", item.Metadata.Function?.Name);
+                return;
+        }
+    }
+
+    private static void HandleStoryBook(GameSession session, Item item) {
+        if (!int.TryParse(item.Metadata.Function?.Parameters, out int storyBookId)){
+            return;
+        }
+        
+        session.Send(StoryBookPacket.Load(storyBookId));
     }
 }
