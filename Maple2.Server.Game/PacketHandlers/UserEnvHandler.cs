e6a92594326e4b4e97bb2c0d2b32dc4a70846854
Zin <62830952+Zintixx@users.noreply.github.com>
2022-11-11 13:49:54 -0800

    Title use scrolls and changing player titles (#6)

---
+++ Maple2.Server.Game/PacketHandlers/UserEnvHandler.cs
@@ -0,0 +1,35 @@
+﻿using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.PacketHandlers;
+using Maple2.Server.Game.Packets;
+using Maple2.Server.Game.Session;
+
+namespace Maple2.Server.Game.PacketHandlers;
+
+public class UserEnvHandler : PacketHandler<GameSession> {
+    public override RecvOp OpCode => RecvOp.RequestUserEnv;
+
+    private enum Command : byte {
+        ChangeTitle = 1,
+    }
+
+    public override void Handle(GameSession session, IByteReader packet) {
+        var command = packet.Read<Command>();
+        switch (command) {
+            case Command.ChangeTitle:
+                HandleChangeTitle(session, packet);
+                break;
+        }
+    }
+
+    private void HandleChangeTitle(GameSession session, IByteReader packet) {
+        int titleId = packet.ReadInt();
+
+        if (titleId != 0 && !session.Player.Value.Unlock.Titles.Contains(titleId)) {
+            return;
+        }
+
+        session.Player.Value.Character.Title = titleId;
+        session.Field?.Broadcast(UserEnvPacket.UpdateTitle(session.Player.ObjectId, titleId));
+    }
+}
