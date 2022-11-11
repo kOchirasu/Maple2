09c9ca17fb63c58b9c87e4e8099294c502619111
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-11-11 00:19:33 -0800

    Add ChangeAttributes+ChangeAttributesScroll packet structs

---
+++ Maple2.Model/Error/ChangeAttributesError.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable InconsistentNaming, IdentifierTypo
+
+using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum ChangeAttributesError {
+    [Description("You do not own this item.")]
+    s_itemremake_error_server_not_in_inven = 1,
+    [Description("This item's bonus attributes cannot be modified.")]
+    s_itemremake_error_server_impossible = 2,
+    [Description("Incorrect item.")]
+    s_itemremake_error_server_null_status = 3, // and 4
+    [Description("Not enough materials.")]
+    s_itemremake_error_server_lack_price = 5,
+    [Description("The Bonus Attribute change failed.")]
+    s_itemremake_error_server_fail_apply_option = 6, // and 7
+    [Description("Cannot lock attribute.")]
+    s_itemremake_error_server_fail_lack_lock_consume_item = 9,
+    [Description("This function has been temporarily restricted.")]
+    s_content_shutdown_notice = 10,
+
+    [Description("Bonus Attributes Modification Error: code=[{0},{1}]")]
+    s_itemremake_error_server_default = byte.MaxValue,
+}
---
+++ Maple2.Server.Game/Packets/ChangeAttributesScrollPacket.cs
@@ -0,0 +1,52 @@
+﻿using Maple2.Model.Error;
+using Maple2.Model.Game;
+using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.Packets;
+using Maple2.Tools.Extensions;
+
+namespace Maple2.Server.Game.Packets;
+
+public static class ChangeAttributesScrollPacket {
+    private enum Command : byte {
+        UseScroll = 0,
+        Preview = 2,
+        Select = 3,
+        Error = 4,
+    }
+
+    public static ByteWriter UseScroll(Item scroll) {
+        var pWriter = Packet.Of(SendOp.ChangeAttributesScroll);
+        pWriter.Write<Command>(Command.UseScroll);
+        pWriter.WriteLong(scroll.Uid);
+
+        return pWriter;
+    }
+
+    public static ByteWriter PreviewItem(Item item) {
+        var pWriter = Packet.Of(SendOp.ChangeAttributesScroll);
+        pWriter.Write<Command>(Command.Preview);
+        pWriter.WriteLong(item.Uid);
+        pWriter.WriteClass<Item>(item);
+
+        return pWriter;
+    }
+
+    public static ByteWriter SelectItem(Item item) {
+        var pWriter = Packet.Of(SendOp.ChangeAttributesScroll);
+        pWriter.Write<Command>(Command.Select);
+        pWriter.WriteLong(item.Uid);
+        pWriter.WriteClass<Item>(item);
+
+        return pWriter;
+    }
+
+    public static ByteWriter Error(ChangeAttributesScrollError error, bool flag = false) {
+        var pWriter = Packet.Of(SendOp.ChangeAttributesScroll);
+        pWriter.Write<Command>(Command.Error);
+        pWriter.WriteBool(flag); // no idea
+        pWriter.Write<ChangeAttributesScrollError>(error);
+
+        return pWriter;
+    }
+}
