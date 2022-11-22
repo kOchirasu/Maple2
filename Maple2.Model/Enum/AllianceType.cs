95000b00ead7d88ab32697607db4a9381bc8b1bd
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-11-21 20:47:33 -0800

    Add Quest packet structs

---
+++ Maple2.Model/Enum/AllianceType.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Maple2.Model.Enum;
+
+public enum AllianceType : short {
+    [Description("none")]
+    AnyFaction = 0,
+    [Description("darkwind")]
+    DarkWind = 1,
+    [Description("mapleunion")]
+    MapleAlliance = 2,
+    [Description("lumiknight")]
+    Lumiknights = 3,
+    [Description("triaroyalguard")]
+    RoyalGuard = 4,
+    [Description("greenhood")]
+    GreenHoods = 5,
+    [Description("mapleunion_kritiasexped")]
+    KritiasMapleAlliance = 6,
+    [Description("lumiknight_kritiasexped")]
+    KritiasLumiknights = 7,
+    [Description("greenhood_kritiasexped")]
+    KritiasGreenHoods = 8,
+    // [Description("georg")]
+    // georg = 9,
+}
---
+++ Maple2.Model/Error/QuestError.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable InconsistentNaming, IdentifierTypo
+
+using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum QuestError {
+    none = 0,
+    [Description("Clear out some space in your inventory.")]
+    s_quest_error_inventory_full = 1,
+    [Description("Failed to complete the quest.")]
+    s_quest_error_consume_fail = 2,
+    [Description("Failed to accept the quest.")]
+    s_quest_error_accept_fail = 3,
+    [Description("This quest has expired.")]
+    s_quest_error_invalid_date = 4,
+    [Description("You cannot receive rewards while tombstoned.")]
+    s_quest_error_fail_complete_by_dead = 5,
+    [Description("")]
+    s_alliance_quest_completion_ticket_error = 7,
+}
