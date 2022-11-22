10d4aa289e2f7cfaf5559d071c3d1d59334a8cc4
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-11-21 20:47:33 -0800

    Add Quest packet structs

---
+++ Maple2.Model/Game/Quest/Quest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Maple2.Model.Enum;
+using Maple2.Model.Metadata;
+using Maple2.PacketLib.Tools;
+using Maple2.Tools;
+
+namespace Maple2.Model.Game;
+
+public class Quest : IByteSerializable {
+    public readonly QuestMetadata Metadata;
+    public int Id => Metadata.Id;
+
+    public QuestState State;
+    public int CompletionCount;
+    public long StartTime;
+    public long EndTime;
+    public long ExpiryTime;
+    public bool Accepted;
+    public IList<int> Conditions;
+
+    public Quest(QuestMetadata metadata) {
+        Metadata = metadata;
+        Conditions = new List<int>();
+    }
+
+    public void WriteTo(IByteWriter writer) {
+        writer.WriteInt(Id);
+        writer.Write<QuestState>(State);
+        writer.WriteInt(CompletionCount);
+        writer.WriteLong(StartTime);
+        writer.WriteLong(EndTime);
+        writer.WriteBool(Accepted);
+
+        writer.WriteInt(Conditions.Count);
+        foreach (int condition in Conditions) {
+            writer.WriteInt(condition);
+        }
+    }
+}
