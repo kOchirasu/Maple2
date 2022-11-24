34f232d565ebcca19254e824674326e53bab9d11
Zin <62830952+Zintixx@users.noreply.github.com>
2022-11-23 23:55:55 -0800

    change mastery packet location

--- Maple2.Server.Game/Manager/MasteryManager.cs
+++ Maple2.Server.Game/Manager/MasteryManager.cs
@@ -2,6 +2,7 @@
 using Maple2.Model.Enum;
 using Maple2.Model.Game;
 using Maple2.Model.Metadata;
+using Maple2.Server.Game.Packets;
 using Maple2.Server.Game.Session;
 
 namespace Maple2.Server.Game.Manager;
