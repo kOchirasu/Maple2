2476b908abe04eaeb8f7b1ebb1ca41b22cfa6d72
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-11-28 12:44:25 -0800

    Add guild packet structs

---
+++ Maple2.Model/Error/GuildError.cs
@@ -0,0 +1,98 @@
+﻿// ReSharper disable InconsistentNaming, IdentifierTypo
+
+using System.ComponentModel;
+
+namespace Maple2.Model.Error;
+
+public enum GuildError : byte {
+    none = 0,
+    [Description("Unknown Guild Error")]
+    s_guild_err_unknown = 1,
+    [Description("Guild not found.")]
+    s_guild_err_null_guild = 3,
+    [Description("This character is already a member of a guild.")]
+    s_guild_err_already_exist = 4,
+    [Description("Unable to send invite.")]
+    s_guild_err_wait_inviting = 5,
+    [Description("Guild invitation failed.")]
+    s_guild_err_blocked = 6,
+    [Description("They have already joined another guild.")]
+    s_guild_err_has_guild = 7,
+    [Description("The guild is no longer valid.")]
+    s_guild_err_invalid_guild = 8,
+    [Description("Unable to invite the player to your guild.")]
+    s_guild_err_null_user = 10,
+    [Description("A guild with the same name already exists.")]
+    s_guild_err_name_exist = 11,
+    [Description("Contains a forbidden word.")]
+    s_guild_err_name_value = 12,
+    [Description("Guild member not found.")]
+    s_guild_err_null_member = 13,
+    [Description("The guild cannot be disbanded if there are any guild members.")]
+    s_guild_err_exist_member = 14,
+    [Description("You have reached the maximum number of guild members.")]
+    s_guild_err_full_member = 15,
+    [Description("This guild member has not joined.")]
+    s_guild_err_not_join_member = 16,
+    [Description("The guild leader cannot leave the guild.")]
+    s_guild_err_cannot_leave_master = 17,
+    [Description("You cannot kick the guild leader.")]
+    s_guild_err_expel_target_master = 18,
+    [Description("To create a guild, you must be above level {0}.")]
+    s_guild_err_not_enough_level = 19,
+    [Description("Not enough mesos.")]
+    s_guild_err_no_money = 20,
+    [Description("You don't have permission to do that.")]
+    s_guild_err_no_authority = 21,
+    [Description("Only the guild leader can do that.")]
+    s_guild_err_no_master = 22,
+    [Description("This rank cannot be used.")]
+    s_guild_err_invalid_grade_range = 23,
+    [Description("You cannot change the maximum amount of guild members to this value.")]
+    s_guild_err_invalid_capacity_range = 24,
+    [Description("This rank cannot be used.")]
+    s_guild_err_invalid_grade_data = 25,
+    [Description("Incorrect rank.")]
+    s_guild_err_invalid_grade_index = 26,
+    [Description("This rank cannot be granted.")]
+    s_guild_err_exist_empty_grade_index = 27,
+    [Description("Rank setting failed.")]
+    s_guild_err_set_grade_failed = 28,
+    [Description("Guild member invitation failed.")]
+    s_guild_err_fail_addmember = 30,
+    [Description("This character was not invited.")]
+    s_guild_err_null_invite_member = 32,
+    [Description("Cannot be done during a guild battle.")]
+    s_guild_err_cant_during_pvp = 33,
+    [Description("The guild member maximum cannot be changed.")]
+    s_guild_extend_capacity_err_cannot = 34,
+    [Description("A problem occurred while changing the maximum amount of guild members.")]
+    s_guild_extend_capacity_err_current = 35,
+    [Description("Preferences have remained the same.")]
+    s_guild_search_same_propensity = 36,
+    [Description("You can submit up to 10.")]
+    s_guild_search_max_join_request = 37,
+    [Description("Please wait a moment.")]
+    s_guild_search_last_request = 38,
+    [Description("Application not found.")]
+    s_guild_search_null_join_guild_request = 39,
+    [Description("The target is in a location where they cannot be invited.")]
+    s_guild_err_fail_this_field = 41,
+    [Description("The guild level is not high enough.")]
+    s_guild_err_not_enough_guild_level = 42,
+    [Description("Not enough guild funds.")]
+    s_guild_err_not_enough_guild_fund = 43,
+    [Description("You cannot use the guild skill right now.")]
+    s_guild_err_cannot_use_skill = 44,
+    [Description("Please try again later.")]
+    s_guild_err_too_fast_to_search_by_name = 45,
+    [Description("You have already arrived at the Glorious Arena.")]
+    s_guild_pvp_already_pvp_field = 46,
+    [Description("Applications are not currently being accepted.")]
+    s_guild_err_isNotVsGameTime = 47,
+    [Description("You need at least {0} players online.")]
+    s_guild_err_requireOnlineUserCount = 48,
+
+    [Description("Undefined Guild Error")]
+    s_guild_err_none = byte.MaxValue,
+}
---
+++ Maple2.Server.Game/PacketHandlers/GuildHandler.cs
@@ -0,0 +1,335 @@
+﻿using Maple2.Model.Enum;
+using Maple2.Model.Game;
+using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.PacketHandlers;
+using Maple2.Server.Game.Session;
+using Maple2.Tools.Extensions;
+using WorldClient = Maple2.Server.World.Service.World.WorldClient;
+
+namespace Maple2.Server.Game.PacketHandlers;
+
+public class GuildHandler : PacketHandler<GameSession> {
+    public override RecvOp OpCode => RecvOp.Guild;
+
+    private enum Command : byte {
+        Create = 1,
+        Disband = 2,
+        Invite = 3,
+        RespondInvite = 4,
+        Leave = 7,
+        Expel = 8,
+        UpdateMemberRank = 10,
+        UpdateMemberMessage = 13,
+        CheckIn = 15,
+        UpdateLeader = 61,
+        UpdateNotice = 62,
+        UpdateEmblem = 63,
+        IncreaseCapacity = 64,
+        UpdateRank = 65,
+        UpdateFocus = 66,
+        SendMail = 69,
+        SendApplication = 80,
+        CancelApplication = 81,
+        RespondApplication = 82,
+        Unknown83 = 83,
+        ListApplications = 84,
+        SearchGuilds = 85,
+        SearchGuildName = 86,
+        UseBuff = 88,
+        UsePersonalBuff = 89,
+        UpgradeBuff = 90,
+        StartArcade = 96,
+        EnterArcade = 97,
+        UpgradeHouseRank = 98,
+        UpgradeHouseTheme = 99,
+        EnterHouse = 100,
+        SendGift = 106,
+        UpdateGiftLog = 109,
+        Donate = 110,
+        UpgradeNpc = 111,
+        CreateGuildEvent = 112,
+        StartGuildEvent = 113,
+        JoinGuildEvent = 117,
+    }
+
+    #region Autofac Autowired
+    // ReSharper disable MemberCanBePrivate.Global
+    public required WorldClient World { private get; init; }
+    // ReSharper restore All
+    #endregion
+
+    public override void Handle(GameSession session, IByteReader packet) {
+        var command = packet.Read<Command>();
+        switch (command) {
+            case Command.Create:
+            HandleCreate(session, packet);
+            return;
+        case Command.Disband:
+            HandleDisband(session);
+            return;
+        case Command.Invite:
+            HandleInvite(session, packet);
+            return;
+        case Command.RespondInvite:
+            HandleRespondInvite(session, packet);
+            return;
+        case Command.Leave:
+            HandleLeave(session);
+            return;
+        case Command.Expel:
+            HandleExpel(session, packet);
+            return;
+        case Command.UpdateMemberRank:
+            HandleUpdateMemberRank(session, packet);
+            return;
+        case Command.UpdateMemberMessage:
+            HandleUpdateMemberMessage(session, packet);
+            return;
+        case Command.CheckIn:
+            HandleCheckIn(session);
+            return;
+        case Command.UpdateLeader:
+            HandleUpdateLeader(session, packet);
+            return;
+        case Command.UpdateNotice:
+            HandleUpdateNotice(session, packet);
+            return;
+        case Command.UpdateEmblem:
+            HandleUpdateEmblem(session, packet);
+            return;
+        case Command.IncreaseCapacity:
+            HandleIncreaseCapacity(session);
+            return;
+        case Command.UpdateRank:
+            HandleUpdateRank(session, packet);
+            return;
+        case Command.UpdateFocus:
+            HandleUpdateFocus(session, packet);
+            return;
+        case Command.SendMail:
+            HandleSendMail(session, packet);
+            return;
+        case Command.SendApplication:
+            HandleSendApplication(session, packet);
+            return;
+        case Command.CancelApplication:
+            HandleCancelApplication(session, packet);
+            return;
+        case Command.RespondApplication:
+            HandleRespondApplication(session, packet);
+            return;
+        case Command.ListApplications:
+            HandleListApplications(session);
+            return;
+        case Command.SearchGuilds:
+            HandleSearchGuilds(session, packet);
+            return;
+        case Command.SearchGuildName:
+            HandleSearchGuildName(session, packet);
+            return;
+        case Command.UseBuff:
+            HandleUseBuff(session, packet);
+            return;
+        case Command.UsePersonalBuff:
+            HandleUsePersonalBuff(session, packet);
+            return;
+        case Command.UpgradeBuff:
+            HandleUpgradeBuff(session, packet);
+            return;
+        case Command.StartArcade:
+            HandleStartArcade(session, packet);
+            return;
+        case Command.EnterArcade:
+            HandleEnterArcade(session, packet);
+            return;
+        case Command.UpgradeHouseRank:
+            HandleUpgradeHouseRank(session, packet);
+            return;
+        case Command.UpgradeHouseTheme:
+            HandleUpgradeHouseTheme(session, packet);
+            return;
+        case Command.EnterHouse:
+            HandleEnterHouse(session);
+            return;
+        case Command.SendGift:
+            HandleSendGift(session, packet);
+            return;
+        case Command.UpdateGiftLog:
+            HandleUpdateGiftLog(session);
+            return;
+        case Command.Donate:
+            HandleDonate(session, packet);
+            return;
+        case Command.UpgradeNpc:
+            HandleUpgradeNpc(session, packet);
+            return;
+        case Command.CreateGuildEvent:
+            HandleCreateGuildEvent(session);
+            return;
+        case Command.StartGuildEvent:
+            HandleStartGuildEvent(session);
+            return;
+        case Command.JoinGuildEvent:
+            HandleJoinGuildEvent(session);
+            return;
+        }
+    }
+
+    private void HandleCreate(GameSession session, IByteReader packet) {
+        string guildName = packet.ReadUnicodeString();
+    }
+
+    private void HandleDisband(GameSession session) {
+
+    }
+
+    private void HandleInvite(GameSession session, IByteReader packet) {
+        string playerName = packet.ReadUnicodeString();
+    }
+
+    private void HandleRespondInvite(GameSession session, IByteReader packet) {
+        var invite = packet.ReadClass<GuildInvite>();
+        bool accepted = packet.ReadBool();
+    }
+
+    private void HandleLeave(GameSession session) {
+
+    }
+
+    private void HandleExpel(GameSession session, IByteReader packet) {
+        string playerName = packet.ReadUnicodeString();
+    }
+
+    private void HandleUpdateMemberRank(GameSession session, IByteReader packet) {
+        string playerName = packet.ReadUnicodeString();
+        byte rankId = packet.ReadByte();
+    }
+
+    private void HandleUpdateMemberMessage(GameSession session, IByteReader packet) {
+        string message = packet.ReadUnicodeString();
+    }
+
+    private void HandleCheckIn(GameSession session) {
+
+    }
+
+    private void HandleUpdateLeader(GameSession session, IByteReader packet) {
+        string leaderName = packet.ReadUnicodeString();
+    }
+
+    private void HandleUpdateNotice(GameSession session, IByteReader packet) {
+        packet.ReadBool();
+        string notice = packet.ReadUnicodeString();
+    }
+
+    private void HandleUpdateEmblem(GameSession session, IByteReader packet) {
+        string emblem = packet.ReadUnicodeString();
+    }
+
+    private void HandleIncreaseCapacity(GameSession session) {
+
+    }
+
+    private void HandleUpdateRank(GameSession session, IByteReader packet) {
+        packet.ReadByte();
+        var rank = packet.ReadClass<GuildRank>();
+    }
+
+    private void HandleUpdateFocus(GameSession session, IByteReader packet) {
+        packet.ReadByte();
+        var focus = packet.Read<GuildFocus>();
+    }
+
+    private void HandleSendMail(GameSession session, IByteReader packet) {
+        string title = packet.ReadUnicodeString();
+        string content = packet.ReadUnicodeString();
+    }
+
+    private void HandleSendApplication(GameSession session, IByteReader packet) {
+        long guildId = packet.ReadLong();
+    }
+
+    private void HandleCancelApplication(GameSession session, IByteReader packet) {
+        long applicationId = packet.ReadLong();
+    }
+
+    private void HandleRespondApplication(GameSession session, IByteReader packet) {
+        long applicationId = packet.ReadLong();
+        bool accepted = packet.ReadBool();
+    }
+
+    private void HandleListApplications(GameSession session) {
+
+    }
+
+    private void HandleSearchGuilds(GameSession session, IByteReader packet) {
+        var focus = packet.Read<GuildFocus>();
+        packet.ReadInt(); // 1
+    }
+
+    private void HandleSearchGuildName(GameSession session, IByteReader packet) {
+        string guildName = packet.ReadUnicodeString();
+    }
+
+    private void HandleUseBuff(GameSession session, IByteReader packet) {
+        int buffId = packet.ReadInt();
+    }
+
+    private void HandleUsePersonalBuff(GameSession session, IByteReader packet) {
+        int buffId = packet.ReadInt();
+    }
+
+    private void HandleUpgradeBuff(GameSession session, IByteReader packet) {
+        int buffId = packet.ReadInt();
+    }
+
+    private void HandleStartArcade(GameSession session, IByteReader packet) {
+        int index = packet.ReadInt();
+    }
+
+    private void HandleEnterArcade(GameSession session, IByteReader packet) {
+        int index = packet.ReadInt();
+    }
+
+    private void HandleUpgradeHouseRank(GameSession session, IByteReader packet) {
+        int rank = packet.ReadInt();
+    }
+
+    private void HandleUpgradeHouseTheme(GameSession session, IByteReader packet) {
+        int theme = packet.ReadInt();
+    }
+
+    private void HandleEnterHouse(GameSession session) {
+
+    }
+
+    private void HandleSendGift(GameSession session, IByteReader packet) {
+        var gift = packet.Read<RewardItem>();
+        string playerName = packet.ReadUnicodeString();
+    }
+
+    private void HandleUpdateGiftLog(GameSession session) {
+
+    }
+
+    private void HandleDonate(GameSession session, IByteReader packet) {
+        int count = packet.ReadInt();
+    }
+
+    private void HandleUpgradeNpc(GameSession session, IByteReader packet) {
+        var type = packet.Read<GuildNpcType>();
+    }
+
+    private void HandleCreateGuildEvent(GameSession session) {
+
+    }
+
+    private void HandleStartGuildEvent(GameSession session) {
+
+    }
+
+    private void HandleJoinGuildEvent(GameSession session) {
+
+    }
+}
