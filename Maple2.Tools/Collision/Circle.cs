374d38021c1b89afd023c695d9736dc5b6f58a36
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-12-03 15:06:25 -0800

    Fix polygon collision for circle/hole circle
    - Add unit tests

--- Maple2.Tools/Collision/Circle.cs
+++ Maple2.Tools/Collision/Circle.cs
@@ -4,16 +4,16 @@ using System.Numerics;
 namespace Maple2.Tools.Collision;
 
 public class Circle : IPolygon {
-    private readonly Vector2 origin;
-    private readonly float radius;
+    public readonly Vector2 Origin;
+    public readonly float Radius;
 
     public Circle(in Vector2 origin, float radius) {
-        this.origin = origin;
-        this.radius = radius;
+        this.Origin = origin;
+        this.Radius = radius;
     }
 
     public virtual bool Contains(in Vector2 point) {
-        return Vector2.DistanceSquared(origin, point) <= radius * radius;
+        return Vector2.DistanceSquared(Origin, point) <= Radius * Radius;
     }
 
     public Vector2[] GetAxes(Polygon? other) {
@@ -24,36 +24,36 @@ public class Circle : IPolygon {
         float min = float.MaxValue;
         int result = 0;
         for (int i = 0; i < other.Points.Length; i++) {
-            float distanceSquared = Vector2.DistanceSquared(origin, other.Points[i]);
+            float distanceSquared = Vector2.DistanceSquared(Origin, other.Points[i]);
             if (distanceSquared < min) {
                 min = distanceSquared;
                 result = i;
             }
         }
 
-        return new[] {other.Points[result] - origin};
+        return new[] {other.Points[result] - Origin};
     }
 
     public Range AxisProjection(Vector2 axis) {
-        float centerProjection = Vector2.Dot(axis, origin);
-        return new Range(centerProjection - radius, centerProjection + radius);
+        float centerProjection = Vector2.Dot(axis, Origin);
+        return new Range(centerProjection - Radius, centerProjection + Radius);
     }
 
     public virtual bool Intersects(IPolygon other) {
         return other switch {
-            Circle circle => IntersectsCircle(circle),
+            Circle circle => circle.IntersectsCircle(this),
             Polygon polygon => polygon.Intersects(this),
             _ => false,
         };
     }
 
     // Optimization to avoid type-check when we already know the shape is a circle.
-    internal bool IntersectsCircle(Circle other) {
-        float maxDistance = radius + other.radius;
-        return Vector2.DistanceSquared(origin, other.origin) <= maxDistance * maxDistance;
+    internal virtual bool IntersectsCircle(Circle other) {
+        float maxDistance = Radius + other.Radius;
+        return Vector2.DistanceSquared(Origin, other.Origin) <= maxDistance * maxDistance;
     }
 
     public override string ToString() {
-        return $"Origin:{origin}, Radius:{radius}";
+        return $"Origin:{Origin}, Radius:{Radius}";
     }
 }
