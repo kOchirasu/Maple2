5e59783639d471780dd844c94b727fd97573509c
Zin <62830952+Zintixx@users.noreply.github.com>
2023-02-03 22:22:08 -0800

    Update GameStorage.Item.cs

--- Maple2.Database/Storage/Game/GameStorage.Item.cs
+++ Maple2.Database/Storage/Game/GameStorage.Item.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Maple2.Database.Extensions;
 using Maple2.Database.Model;
 using Maple2.Model.Enum;
-using Maple2.Model.Game.Event;
 using Maple2.Model.Metadata;
 using Item = Maple2.Model.Game.Item;
 using PetConfig = Maple2.Model.Game.PetConfig;
-using TrafficOptimizer = Maple2.Model.Game.Event.TrafficOptimizer;
 using UgcItemLook = Maple2.Model.Game.UgcItemLook;
 
 namespace Maple2.Database.Storage;
