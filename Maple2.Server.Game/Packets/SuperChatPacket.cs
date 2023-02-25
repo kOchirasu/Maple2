dbe19953bfa4009f7766d7e2e986f714b6e6b32a
Zin <62830952+Zintixx@users.noreply.github.com>
2023-02-25 00:16:31 -0800

    Super Chat Handler (#24)

---
+++ Maple2.Server.Game/Packets/SuperChatPacket.cs
@@ -0,0 +1,29 @@
+﻿using Maple2.PacketLib.Tools;
+using Maple2.Server.Core.Constants;
+using Maple2.Server.Core.Packets;
+
+namespace Maple2.Server.Game.Packets;
+
+public static class SuperChatPacket {
+    private enum Command : byte {
+        Select = 0,
+        Deselect = 1,
+    }
+
+    public static ByteWriter Select(int objectId, int itemId) {
+        var pWriter = Packet.Of(SendOp.SuperWorldChat);
+        pWriter.Write<Command>(Command.Select);
+        pWriter.WriteInt(objectId);
+        pWriter.WriteInt(itemId);
+
+        return pWriter;
+    }
+    
+    public static ByteWriter Deselect(int objectId) {
+        var pWriter = Packet.Of(SendOp.SuperWorldChat);
+        pWriter.Write<Command>(Command.Deselect);
+        pWriter.WriteInt(objectId);
+
+        return pWriter;
+    }
+}
