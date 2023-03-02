c2f3b376f022d448084cba7ab290a7d094e80192
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2023-03-01 20:37:50 -0800

    Remove extra spaces

--- Maple2.Model/Game/RewardItem.cs
+++ Maple2.Model/Game/RewardItem.cs
@@ -6,12 +6,12 @@ namespace Maple2.Model.Game;
 [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 14)]
 public readonly struct RewardItem {
     public int ItemId { get; }
-    public short Rarity  { get; }
-    public int Amount  { get; }
-    public bool Unknown1  { get; }
-    public bool Unknown2  { get; }
-    public bool Unknown3  { get; }
-    public bool Unknown4  { get; }
+    public short Rarity { get; }
+    public int Amount { get; }
+    public bool Unknown1 { get; }
+    public bool Unknown2 { get; }
+    public bool Unknown3 { get; }
+    public bool Unknown4 { get; }
 
     [JsonConstructor]
     public RewardItem(int itemId, short rarity, int amount) {
