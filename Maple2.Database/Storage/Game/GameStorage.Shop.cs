6777c3154817da1d6e90bb46274e0952cd132728
Zin <62830952+Zintixx@users.noreply.github.com>
2023-05-31 09:33:13 -0700

    Update GameStorage.Shop.cs

--- Maple2.Database/Storage/Game/GameStorage.Shop.cs
+++ Maple2.Database/Storage/Game/GameStorage.Shop.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Maple2.Model.Game;
 using Maple2.Model.Game.Shop;
 
 namespace Maple2.Database.Storage;
 
 public partial class GameStorage {
     public partial class Request {
-        public Shop? GetShop(int shopId) => Context.Shop.Find(shopId);
+        public Shop? GetShop(int shopId) {
+            Shop? shop = Context.Shop.Find(shopId);
+            if (shop == null) {
+                return null;
+            }
+            return shop;
+        }
 
         public IList<ShopItem> GetShopItems(int shopId) {
             return Context.ShopItem.Where(model => model.ShopId == shopId)
