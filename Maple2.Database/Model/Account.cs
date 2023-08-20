d2508ebee79696e434fe4eef0d0b362af6dffea6
Zin <62830952+Zintixx@users.noreply.github.com>
2023-08-20 01:29:14 -0700

    Update Account.cs

--- Maple2.Database/Model/Account.cs
+++ Maple2.Database/Model/Account.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Maple2.Database.Extensions;
 using Maple2.Model.Enum;
 using Maple2.Model.Game;
