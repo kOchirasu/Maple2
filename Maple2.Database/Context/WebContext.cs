463f16d46cb82450bbe92b7b1bd4ddd77df93d60
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2023-08-26 19:05:04 -0700

    Add WebStorage for UgcResource (#49)

---
+++ Maple2.Database/Context/WebContext.cs
@@ -0,0 +1,15 @@
+﻿using Maple2.Database.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Maple2.Database.Context;
+
+public sealed class WebContext : DbContext {
+    internal DbSet<UgcResource> UgcResource { get; set; } = null!;
+
+    public WebContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UgcResource>(Maple2.Database.Model.UgcResource.Configure);
+    }
+}
