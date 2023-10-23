74abd338fd7aa38c00cb1cff53ac7b64da67bb6a
Zin <62830952+Zintixx@users.noreply.github.com>
2023-10-22 23:08:59 -0700

    Buffs: part 2 (#63)

--- Maple2.Server.Game/Session/GameSession.cs
+++ Maple2.Server.Game/Session/GameSession.cs
@@ -148,7 +148,6 @@ public sealed partial class GameSession : Core.Network.Session {
             Send(MigrationPacket.MoveResult(MigrationError.s_move_err_default));
             return false;
         }
-        Player.Buffs.Initialize();
 
         var playerUpdate = new PlayerUpdateRequest {
             AccountId = accountId,
@@ -257,7 +256,6 @@ public sealed partial class GameSession : Core.Network.Session {
         NpcScript = null;
 
         if (Field != null) {
-            Player.Buffs.LeaveField();
             Scheduler.Stop();
             Field.RemovePlayer(Player.ObjectId, out _);
         }
@@ -279,6 +277,7 @@ public sealed partial class GameSession : Core.Network.Session {
 
         Field = newField;
         Player = Field.SpawnPlayer(this, Player, portalId, position, rotation);
+        Config.Skill.UpdatePassiveBuffs();
         Player.Buffs.LoadFieldBuffs();
 
         return true;
@@ -327,7 +326,7 @@ public sealed partial class GameSession : Core.Network.Session {
         Send(RevivalPacket.Count(0)); // TODO: Consumed daily revivals?
         Send(RevivalPacket.Confirm(Player));
         Config.LoadStatAttributes();
-        Player.Buffs.Initialize();
+        Player.Buffs.LoadFieldBuffs();
         Send(PremiumCubPacket.Activate(Player.ObjectId, Player.Value.Account.PremiumTime));
         Send(PremiumCubPacket.LoadItems(Player.Value.Account.PremiumRewardsClaimed));
         ConditionUpdate(ConditionType.map, codeLong: Player.Value.Character.MapId);
