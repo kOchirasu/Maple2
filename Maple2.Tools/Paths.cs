be628e15571859dc52a4971ca085a4590f8d8951
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2024-04-26 21:37:58 -0300

    feat: simplify db setup (#4)

--- Maple2.Tools/Paths.cs
+++ Maple2.Tools/Paths.cs
@@ -5,5 +5,7 @@ namespace Maple2.Tools;
 public static class Paths {
     public static readonly string SOLUTION_DIR = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../.."));
 
-    public static readonly string GAME_SCRIPTS_DIR = $"{SOLUTION_DIR}/Maple2.Server.Game/Scripts";
+    public static readonly string GAME_SCRIPTS_DIR = Path.Combine(SOLUTION_DIR, "Maple2.Server.Game", "Scripts");
+
+    public static readonly string DB_SEEDS_DIR = Path.Combine(SOLUTION_DIR, "Maple2.Database.Seed", "Seeds");
 }
