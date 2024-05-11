fa4816ef9e9555b1d573ad44388d80ba849e0053
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2024-05-10 21:14:16 -0700

    Add AI comment translation util (#96)

---
+++ Maple2.File.Ingest/Utils/AiTranslate.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace Maple2.File.Ingest.Utils;
+
+public static class AiTranslate {
+    private static readonly List<(string Kr, string En)> Lookup = new();
+
+    static AiTranslate() {
+        using var reader = new StreamReader("Utils/ai_translate.csv");
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        csv.Read();
+        csv.ReadHeader();
+
+        while (csv.Read()) {
+            string kr = csv.GetField("kr");
+            string en = csv.GetField("en");
+            Lookup.Add((kr, en));
+        }
+    }
+
+    public static string Translate(string input) {
+        // Only translate if there is at least one non-ascii character.
+        if (!input.Any(c => c > 0x255)) {
+            return input;
+        }
+
+        foreach ((string kr, string en) in Lookup) {
+            if (input.Contains(kr)) {
+                return input.Replace(kr, en);
+            }
+        }
+
+        Console.WriteLine($"No translation for: {input.Trim()}");
+        return input;
+    }
+}
